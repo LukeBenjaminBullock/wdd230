8ccc8d56f3983657666b293605b448a2360a60a4
LukeBenjaminBullock <bul22006@byui.edu>
2023-02-06 15:29:23 -0700

    first rendition

---
+++ projects/chamber/scripts/windchill.js
@@ -0,0 +1,17 @@
+var temp = 20;
+var windSpeed = 4;
+
+function calculateWindChill(temp, windSpeed) {
+    if (temp <= 50 && windSpeed > 3) {
+        var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
+    return windChill.toFixed(2);
+    } else {
+        return "N/A";
+    }
+}
+
+var windChill = calculateWindChill(temp, windSpeed);
+
+document.querySelector("#wind-temp").innerHTML = temp + " &#176;C";
+document.querySelector("#wind-speed").innerHTML = "Windspeed: " + windSpeed + "km/h";
+document.querySelector("#wind-chill").innerHTML = "Windchill: " + windChill;
