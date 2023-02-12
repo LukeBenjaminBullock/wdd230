ad72db8d68e05ce7612f2c171e0fc4dfe5ad4312
LukeBenjaminBullock <bul22006@byui.edu>
2023-02-12 16:46:35 -0700

    Added a link to my work in progress

--- projects/chamber/scripts/discover.js
+++ projects/chamber/scripts/discover.js
@@ -10,3 +10,21 @@ const loadImages = (image) => {
 imagesToLoad.forEach((img) => {
     loadImages(img);
 });
+
+// ! Display time since last visit 
+
+var lastVisit = localStorage.getItem('lastVisit');
+var currentTime = Date.now();
+
+localStorage.clear()
+console.log(localStorage)
+
+    if (!lastVisit) {
+        localStorage.setItem('lastVisit', currentTime);
+        document.querySelector('.visit-time').textContent = "This is your first visit!";
+    } else {
+        var timeSinceLastVisit = currentTime - lastVisit;
+        var daysSinceLastVisit = Math.round(timeSinceLastVisit / (1000 * 60 * 60 * 24));
+        document.querySelector('.visit-time').textContent = `You last visited ${daysSinceLastVisit} days ago.`;
+        localStorage.setItem('lastVisit', currentTime);
+    }
